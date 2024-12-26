ebf562f14e74890d1f5676c2cbb9a25c0e4c96c2
Spectre@18 <ashish.karn43@gmail.com>
2024-12-26 20:48:23 +0530

    Main file commit 1 test

--- BookMyShow/src/Main.java
+++ BookMyShow/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         BookMyShow bms = new BookMyShow();
         bms.hello();
-        System.out.println("Hello worldthtt!");
+        System.out.println("Hello worldthttt!");
     }
 }
